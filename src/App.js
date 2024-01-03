c82187ecbe34ba82b9100831926f1504e27d72fe
“sangjun121” <juncho12011201@gmail.com>
2024-01-03 23:41:56 +0900

    feat: 캐릭터 생성 프롬포트 페이지 구현 완료

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import MainPage from "./pages/MainPage";
-import LoginPage from "./pages/LoginPage";
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import MainPage from './pages/MainPage';
+import LoginPage from './pages/LoginPage';
+import CharacterPage from './pages/CharacterPage';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/login" element={<LoginPage />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <Routes>
+                    <Route path="/" element={<MainPage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/character" element={<CharacterPage />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
