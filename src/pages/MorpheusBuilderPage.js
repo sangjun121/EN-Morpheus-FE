140ca1034b7cc23441b4c26b786cb6e9cdb74e54
ohsung0722 <99doldol@gmail.com>
2024-01-28 03:58:40 +0900

    [ADD] 캐릭터와 주제를 모두 선택했을 때만 시나리오 생성되도록 설정

--- src/pages/MorpheusBuilderPage.js
+++ src/pages/MorpheusBuilderPage.js
@@ -13,6 +13,7 @@ const MorpheusBuilderPage = () => {
   const [isThemeBoxExpanded, setIsThemeBoxExpanded] = useState(false);
   const [isCharacterBoxExpanded, setIsCharacterBoxExpanded] = useState(false);
   const [topic, setTopic] = useState("");
+  const [characterId, setCharacterId] = useState();
 
   const handleLoadingFinished = () => {
     setLoading(false);
@@ -35,7 +36,23 @@ const MorpheusBuilderPage = () => {
 
   //캐릭터 ID 하위 컴포넌트에서 받아오는 함수
   const onCharacterSelected = (characterId) => {
+    setCharacterId(characterId);
     console.log("Selected character Id:", characterId);
+  };
+
+  //캐릭터와 주제를 선택했는지 확인하고 선택했을 때만 다음 이벤트 발생시키는 로직
+  const navigateToDraft = () => {
+    if (!topic && !characterId) {
+      alert("Please select both a theme and a character before proceeding.");
+      return;
+    } else if (!topic) {
+      alert("Please select a theme before proceeding.");
+      return;
+    } else if (!characterId) {
+      alert("Please select a character before proceeding.");
+      return;
+    }
+    navigate("./scenario-draft");
   };
 
   return (
@@ -64,10 +81,7 @@ const MorpheusBuilderPage = () => {
       </div>
       {!(isThemeBoxExpanded || isCharacterBoxExpanded) && (
         <div>
-          <button
-            className="draft-button"
-            onClick={() => navigate("./scenario-draft")}
-          >
+          <button className="draft-button" onClick={navigateToDraft}>
             AI Scenario Draft
           </button>
         </div>
